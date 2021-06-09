a758954a3ac66b2787e5bdfec7316860a1c746ad
Pineapple84 <60586178+Pineapple84@users.noreply.github.com>
2021-06-09 17:18:46 +0700

    Create count_alphab.py

---
+++ Python/Courses/Count word occurrence in string (2 Exercises)/Count word in string (Alphabetically)/count_alphab.py
@@ -0,0 +1,15 @@
+# Count the longest string alphabetically -> 'beggh'
+
+alphabet = 'abcdefgkijlmnopqrstuvwxyz'
+s = 'azcbobobegghakl'
+string = ""
+count = 0
+
+while count <= len(alphabet):
+    count += 1
+    find_b = s.find('b')
+    if s[find_b] <= s[find_b+1]:
+        string = s[find_b] + s[find_b+1:s.find('h')+1]
+    else:
+        string = s[find_b]
+print(string)
