9bc136970068863b869523c22d8fdc9f7fedfdfc
Pineapple84 <60586178+Pineapple84@users.noreply.github.com>
2021-06-10 08:59:43 +0700

    Update count_alphab.py

--- Python/Courses/Count word occurrence in string (2 Exercises)/Count word in string (Alphabetically)/count_alphab.py
+++ Python/Courses/Count word occurrence in string (2 Exercises)/Count word in string (Alphabetically)/count_alphab.py
@@ -9,7 +9,7 @@ while count <= len(alphabet):
     count += 1
     find_b = s.find('b')
     if s[find_b] <= s[find_b+1]:
-        string = s[find_b] + s[find_b+1:s.find('h')+1]
+        string = s[find_b+4:s.find('h')+1]    
     else:
         string = s[find_b]
 print(string)
