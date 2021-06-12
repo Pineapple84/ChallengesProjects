2c9a4bf975c81fe5e1a3ad80c899eb25082b5233
Pineapple84 <60586178+Pineapple84@users.noreply.github.com>
2021-06-12 22:08:35 +0700

    Update half_guessing_number.py

--- Python/Courses/Find the half of guessing number/half_guessing_number.py
+++ Python/Courses/Find the half of guessing number/half_guessing_number.py
@@ -2,16 +2,23 @@
 # IN PROGRESS...
 
 half = 50
-guess = input("Guess your number: ")
+part = 4
+guess = int(input("Guess your number: "))
 
 for guess in range(100):
-    print("Is your number is higher or lower?")
-    case 'higher':
-        half_02 = half + (guess / 2)
-    case 'lower':
-        half_02 = guess / 4
-    case 'equal:
-        print("Congrats!!!, We got " + guess)
+    ans = input("Is your number is higher or lower than " + str(half) + " ?")
+    
+    if ans == "higher":
+        value_01 = half + (half / 2)
+        print(value_01)
+        
+    while part <= 200:
+        ans_02 = input("Is number is higher or lower than previous number" + " ?")
+        if ans_02 == "higher":
+            new_val = value_01 + (value_01 / part)
+            print(new_val)
+            value_01 = new_val
+        part *= 2
         
     if guess <= -1 or guess > 100:
         break
